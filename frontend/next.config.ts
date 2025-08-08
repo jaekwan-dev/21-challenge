9dc63d1cad19d2718725bce2df0f9dbd0489aac2
jaekwan-dev <jaekwan.yoodev@gmail.com>
2025-08-08 10:50:24 +0900

    image domin 변경

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['k.kakaocdn.net'],
+  },
 };
 
 export default nextConfig;
